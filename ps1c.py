a865b0637ae39b4246a314db6de2ef50760f4356
Harishankar S <58472023+HurryNair@users.noreply.github.com>
2020-04-01 20:21:43 +0530

    Update ps1c.py

--- ps1c.py
+++ ps1c.py
@@ -1,16 +1,7 @@
 def savings_calculator(annual_salary):
-    total_cost = 1000000
-    semi_annual_raise = 0.07
-    portion_down_payment = 0.25
-    r = 0.04
-
-    target = total_cost*portion_down_payment
-    portion_saved = 0.5
-    cnt = 0
-
-    while True:
+    while True:    
         savings = calculate_savings(portion_saved, 0, annual_salary, r, semi_annual_raise)
-
+        
         if savings < target - 100:
             portion_saved = (portion_saved*2 + portion_saved) /2
             cnt += 1
@@ -37,8 +28,19 @@ def calculate_savings(portion_saved, current_savings, annual_salary, r, semi_ann
 
 def main():
     annual_salary = int(input("Enter your annual salary: "))
-    print("Best savings rate: ", savings_calculator(annual_salary)[0])
-    print("Steps in bisection search: ", savings_calculator(annual_salary)[1])
+    total_cost = 1000000
+    semi_annual_raise = 0.07
+    portion_down_payment = 0.25
+    r = 0.04
+    target = total_cost*portion_down_payment
+    portion_saved = 0.5
+    cnt = 0
+    
+    if (calculate_savings(0.6, 0, annual_salary, r, semi_annual_raise) != target):
+            print("It is not possible to pay the down payment in three years.")
+    else:
+        print("Best savings rate: ", savings_calculator(annual_salary, total_cost, semi_annual_raise, portion_down_payment, r, target, portion_saved, cnt)[0])
+        print("Steps in bisection search: ", savings_calculator(annual_salary, total_cost, semi_annual_raise, portion_down_payment, r, target, portion_saved, cnt)[1])
 
 
 if __name__ == "__main__":
